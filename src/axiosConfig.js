87fff82684bd139d808fde148165d30201e83dec
VAALEJANDR0 <rventura719@gmail.com>
2024-08-09 16:34:00 -0600

    Ajustes

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://192.168.1.196/api', 
+  baseURL: 'http://localhost:9000/api', 
   headers: {
     'Content-Type': 'application/json',
   },
